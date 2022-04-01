a25bfb120fca901278a076d198a2ccd1c752de5f
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-04-01 09:38:18 +0200

    added eslint

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
 const responses = [
-	"Yes",
-	"No",
-	"True",
-	"False",
-	"Hit",
-	"Miss",
-	"Definitely",
-	"Definitely not",
-	"Absolutely",
-	"Absolutely not",
-	"Yep",
-	"Nope",
-	"Positive",
-	"Negative"
-]
+	'Yes',
+	'No',
+	'True',
+	'False',
+	'Hit',
+	'Miss',
+	'Definitely',
+	'Definitely not',
+	'Absolutely',
+	'Absolutely not',
+	'Yep',
+	'Nope',
+	'Positive',
+	'Negative',
+];
 
 client.once('ready', () => {
-    client.user.setPresence({ activities: [{ name: 'Yes?' }], status: 'online' });
-    console.log("yes?");
+	client.user.setPresence({ activities: [{ name: 'Yes?' }], status: 'online' });
+	console.log('yes?');
 });
-client.on('messageCreate',message => {
-    if (message.author.bot) return;
-    let yes = message.content.toLowerCase()
-    if (yes == 'ben') {message.channel.send({
-        files: ['./ben.png'],
-        content: 'Ben'
-    }); return;}
-    if (yes.startsWith('ben')) {
-      if (yes == 'ben github' || yes == 'ben code') {message.channel.send('https://github.com/qweri0p/talking-ben-discord'); return;}
-        let randomWord = Math.floor(Math.random() * responses.length);
-	message.channel.send(responses[randomWord]);
-	return;
-    }
-    if (yes.startsWith('sugoma')) {
-        const sugomaEmbed = new Discord.MessageEmbed()
-            .setTitle("sougon deez nuts")
-            .setDescription("sus")
-            .setColor("#ffffff")
-            .setURL('https://sugoma.xyz/')
-            .setImage('https://sugoma.xyz/content/sumoga.jpg')
-            .setFooter({text: "sussiest dog alive"})
-        message.channel.send({ embeds:[sugomaEmbed]
-    });
-    }
-    if (message.content.startsWith('updateben')) {
-        if (message.author.id == '354943770464354306') {
-            process.exit(0);
-        } else {
-            message.channel.send('No!');
-        }
-    }
+client.on('messageCreate', message => {
+	if (message.author.bot) return;
+	const yes = message.content.toLowerCase();
+	if (yes == 'ben') {
+		message.channel.send({
+			files: ['./ben.png'],
+			content: 'Ben',
+		}); return;
+	}
+	if (yes.startsWith('ben')) {
+		if (yes == 'ben github' || yes == 'ben code') {
+			message.channel.send('https://github.com/qweri0p/talking-ben-discord');
+			return;
+		}
+		const randomWord = Math.floor(Math.random() * responses.length);
+		message.channel.send(responses[randomWord]);
+		return;
+	}
+	if (yes.startsWith('sugoma')) {
+		const sugomaEmbed = new Discord.MessageEmbed()
+			.setTitle('sougon deez nuts')
+			.setDescription('sus')
+			.setColor('#ffffff')
+			.setURL('https://sugoma.xyz/')
+			.setImage('https://sugoma.xyz/content/sumoga.jpg')
+			.setFooter({ text: 'sussiest dog alive' });
+		message.channel.send({ embeds:[sugomaEmbed] });
+	}
+	if (message.content.startsWith('updateben')) {
+		if (message.author.id == '354943770464354306') {
+			process.exit(0);
+		}
+		else {
+			message.channel.send('No!');
+		}
+	}
 });
 
 client.login(config.token);
